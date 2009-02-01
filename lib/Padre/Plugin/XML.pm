7a44222d35a699f86523736eb1e68d353400c8e2
Fayland 林 <fayland@gmail.com>
2009-02-01 15:28:22 +0000

    Import of FAYLAND/Padre-Plugin-XML-0.05 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-XML
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         FAYLAND/Padre-Plugin-XML-0.05.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/Padre/Plugin/XML.pm
+++ lib/Padre/Plugin/XML.pm
@@ -3,13 +3,13 @@ package Padre::Plugin::XML;
 use warnings;
 use strict;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use base 'Padre::Plugin';
 use Wx ':everything';
 
 sub padre_interfaces {
-	'Padre::Plugin'   => 0.23,
+	'Padre::Plugin'   => 0.26,
 	'Padre::Document' => 0.21,
 }
 
@@ -18,31 +18,66 @@ sub registered_documents {
 }
 
 sub menu_plugins_simple {
-	'XML' => [
-		'Tidy XML', \&tidy_xml,
-	];
+	my $self = shift;
+	return ('XML' => [
+		'Tidy XML', sub { $self->tidy_xml },
+	]);
 }
 
 sub tidy_xml {
 	my ( $self ) = @_;
 	
-	my $src = $self->current->text;
-	my $doc = $self->current->document;
+	my $main = $self->main;
+	
+	my $src = $main->current->text;
+	my $doc = $main->current->document;
+
+	unless ( $doc and $doc->isa('Padre::Document::XML') ) {
+		$main->message( Wx::gettext("This is not a XML document!") );
+		return;
+	}
+
 	my $code = ( $src ) ? $src : $doc->text_get;
 	
 	return unless ( defined $code and length($code) );
 	
 	require XML::Tidy;
-	my $tidy_obj = XML::Tidy->new( xml => $code );
-	$tidy_obj->tidy();
+
+	my $tidy_obj = '';
+	my $string   = '';
+	eval {
+		$tidy_obj = XML::Tidy->new( xml => $code );
+		$tidy_obj->tidy();
 	
-	my $string = $tidy_obj->toString();
-	if ( $src ) {
-		my $editor = $self->current->editor;
-		$editor->ReplaceSelection( $string );
-	} else {
-		$doc->text_set( $string );
+		$string = $tidy_obj->toString();
+	};
+
+	if ( ! $@ ) {
+		if ( $src ) {
+			$string =~ s/\A<\?xml.+?\?>\r?\n?//o;
+			my $editor = $main->current->editor;
+			$editor->ReplaceSelection( $string );
+		} else {
+			$doc->text_set( $string );
+		}
 	}
+	else {
+		$main->message( Wx::gettext("Tidying failed due to error(s):") . "\n\n" . $@ );
+	}
+
+	return;
+}
+
+sub editor_enable {
+	my $self     = shift;
+	my $editor   = shift;
+	my $document = shift;
+
+	if ( $document->isa('Padre::Document::XML') ) {
+		$editor->SetProperty('fold.html', '1');
+	}
+
+	return 1;
 }
 
 1;
@@ -59,6 +94,8 @@ use L<XML::Tidy> to tidy XML
 =head1 AUTHOR
 
 Fayland Lam, C<< <fayland at gmail.com> >>
+
+Heiko Jansen, C<< <heiko_jansen@web.de> >>
 
 =head1 COPYRIGHT & LICENSE
 
